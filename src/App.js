62debfafea811a1a8addef960b12d299b07c4efb
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-25 18:00:56 +0530

    Added the game logic

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mins: "2",
+      seconds: "59",
       allFliped: false,
       isGameStart: false,
       isTimerStart: false,
+      isGameFinish : false,
       jumbeledArray: "",
-      isGameOver : false
+      isGameOver : false,
+      previousIndex : -1
     };
   }
 
@@ -38,8 +42,17 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    let jumbeledArray = this.shuffle().split("");
+    let jumbeledArrayUpdated = jumbeledArray.map((data,index) => {
+        return {
+          ID :index,
+          CLUE : data,
+          ISFLIPPED : false,
+          ISDISABLED : false
+        }
+    });
     this.setState({
-      jumbeledArray: this.shuffle().split("")
+      jumbeledArray: jumbeledArrayUpdated
     });
   }
 
@@ -58,13 +71,78 @@ class App extends React.Component {
         this.setState({
           allFliped: false,
           isTimerStart : false
+        },() => {
+          setInterval(this.timer, 1000);
         });
       }, 3000);
     });
   }
 
+  timer = () => {
+    let seconds = this.state.seconds;
+    let minute = this.state.mins;
+    if (seconds === "00") {
+      seconds = "59";
+      minute = parseInt(minute) - 1;
+    } else {
+      seconds = parseInt(seconds) - 1;
+    }
+
+    if (parseInt(seconds) < 10) {
+      seconds = "0" + seconds
+    }
+
+    if (!this.state.isGameFinish && !this.state.isGameOver) {
+      if (minute === "0" && seconds === "00") {
+        this.setState({
+          seconds: seconds.toString(),
+          isGameOver: true,
+          isGameFinish : true
+        })
+      } else {
+        this.setState({
+          seconds: seconds.toString(),
+          mins: minute.toString()
+        })
+      }
+    }
+  }
+
   singleFliped = (index) => {
-    console.log(index);
+    let previousIndex = this.state.previousIndex;
+    let jumbeledArray = this.state.jumbeledArray;
+    jumbeledArray[index].ISFLIPPED = true;
+    if(this.state.previousIndex !== -1)
+    {
+      if(jumbeledArray[index].CLUE === jumbeledArray[previousIndex].CLUE){
+        jumbeledArray[index].ISDISABLED = true;
+        jumbeledArray[previousIndex].ISDISABLED = true;
+        this.setState({
+          previousIndex: -1,
+          jumbeledArray: jumbeledArray
+        });
+      }else{
+        jumbeledArray[index].ISFLIPPED = true;
+        this.setState({
+          previousIndex:-1,
+          jumbeledArray: jumbeledArray
+        },() =>{
+          setTimeout(() => {
+            jumbeledArray[index].ISFLIPPED = false;
+            jumbeledArray[previousIndex].ISFLIPPED = false;
+            this.setState({
+              jumbeledArray: jumbeledArray
+            });
+          }, 1000);
+        });
+      }
+    }else{
+      jumbeledArray[index].ISFLIPPED = true;
+      this.setState({
+        previousIndex: index,
+        jumbeledArray: jumbeledArray
+      })
+    }
   }
 
   render() {
@@ -76,13 +154,13 @@ class App extends React.Component {
               <div className="row">
                 <div className="col-md-12 contentBox d-flex justify-content-center align-items-center align-content-around flex-wrap">
                   {this.state.jumbeledArray && this.state.jumbeledArray.map((data, index) => {
-                    return <div class={"flip-box" + (this.state.allFliped ? " flipped" : "")} onClick={() => this.singleFliped(index)}>
+                    return <div class={"flip-box" + (this.state.allFliped && this.state.isGameStart ? " flipped" : "") + (data.ISFLIPPED  && this.state.isGameStart ? " flipped" : "")} onClick={() => this.singleFliped(index)}>
                       <div class="flip-box-inner">
                         <div class="flip-box-front">
                           <h2>&nbsp;</h2>
                         </div>
                         <div class="flip-box-back">
-                          <h2>{data}</h2>
+                          <h2>{data.CLUE}</h2>
                         </div>
                       </div>
                     </div>
@@ -97,8 +175,8 @@ class App extends React.Component {
                   <br />
                   <br />
                   {!this.state.isGameStart && <p><button className="gamebutton" onClick={this.startGame}>START GAME</button></p>}
-                  {this.state.isGameStart && this.state.isGameOver && <p><button className="gamebutton">RESTART GAME</button></p>}
-                  {this.state.isGameStart && <p><button className="timerbutton">00 : 00</button></p>}
+                  {this.state.isGameStart && this.state.isGameOver && this.state.isGameFinish && <p><button className="gamebutton">RESTART GAME</button></p>}
+                  {this.state.isGameStart && <p><button className="timerbutton">{"0" + this.state.mins + " : " + this.state.seconds}</button></p>}
                 </div>
               </div>
             </div>
